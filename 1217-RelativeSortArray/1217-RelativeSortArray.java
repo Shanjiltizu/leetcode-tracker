da86b7b38cacba309891d7704bded3c7434a5cda
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-07-09 09:49:59 +0530

    Create 1217-RelativeSortArray

---
+++ 1217-RelativeSortArray/1217-RelativeSortArray.java
@@ -0,0 +1,29 @@
+// Last updated: 09/07/2026, 09:49:59
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+     int res[]=new int[arr1.length];
+     int index=0;
+    List<Integer>list=new ArrayList<>();
+     for(int i=0;i<arr2.length;i++){
+        for(int j=0;j<arr1.length;j++){
+            if(arr2[i]==arr1[j]){
+                list.add(arr1[j]);
+            }
+        }
+     }
+     List<Integer>ans=new ArrayList<>();
+     for(int i:arr1){
+        if(!list.contains(i)){
+            ans.add(i);
+        }
+     }
+     Collections.sort(ans);
+     for(int k:ans){
+        list.add(k);
+     }
+     for(int k:list){
+        res[index++]=k;
+     }
+     return res;
+    }
+}
