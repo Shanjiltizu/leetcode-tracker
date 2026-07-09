e14e79a1fb077b208f6c84b2bff43296596bfdc7
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-07-09 09:51:27 +0530

    Create 172-FactorialTrailingZeroes

---
+++ 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.java
@@ -0,0 +1,14 @@
+// Last updated: 09/07/2026, 09:51:26
+class Solution {
+    public int trailingZeroes(int n) {
+        
+        int count = 0;
+        
+        while(n > 0) {
+            n = n / 5;
+            count += n;
+        }
+        
+        return count;
+    }
+}
