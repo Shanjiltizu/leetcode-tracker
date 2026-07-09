0de1bd8e4bcf1d1cc95d60922bc8ed2e7912e392
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-07-09 09:50:08 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,13 @@
+// Last updated: 09/07/2026, 09:50:08
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+        }
+     return -1;   
+    }
+}
