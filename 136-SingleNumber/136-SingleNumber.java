7c0209c669213e1626c24d53aeb3f9eb38f0061c
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-07-09 09:51:38 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 09/07/2026, 09:51:37
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result =0;
+        for(int num:nums)
+        {
+            result^=num;
+
+        }
+        return result;
+        
+    }
+}
