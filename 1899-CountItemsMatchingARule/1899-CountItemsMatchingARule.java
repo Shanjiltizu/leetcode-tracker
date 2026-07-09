3c06eabbb5c5f5cad215fac3bf1638ff4c54aea5
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-07-09 09:49:39 +0530

    Create 1899-CountItemsMatchingARule

---
+++ 1899-CountItemsMatchingARule/1899-CountItemsMatchingARule.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:49:39
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+     int key = 0; 
+    int count = 0;
+
+    switch (ruleKey) {
+        case "type": key = 0; break;
+        case "color": key = 1; break;
+        case "name": key = 2; break;
+    }
+
+    for (List<String> item : items) {
+        if (item.get(key).equals(ruleValue)) {
+            count++;
+        }
+    }
+    return count;
+    }
+}
