38d105d5ba7596012e7d6cadb3a9c6b9cb3f25f3
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-07-09 09:49:45 +0530

    Create 1547-DestinationCity

---
+++ 1547-DestinationCity/1547-DestinationCity.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:49:44
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Map<String,Integer> mp = new HashMap<>();
+
+        for(List<String> path : paths){
+            String source = path.get(0);
+            mp.put(source,mp.getOrDefault(source,0)+1);
+
+        } 
+        for(List<String> path : paths){
+            String dest = path.get(1);
+
+            if(mp.getOrDefault(dest,0) == 0){
+                return dest;
+            }
+        }
+        return ""; 
+    }
+}
