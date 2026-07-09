8732115f598303aa0055b63138c05f629080a92c
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-07-09 09:51:56 +0530

    Create 99-RecoverBinarySearchTree

---
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -0,0 +1,50 @@
+// Last updated: 09/07/2026, 09:51:55
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        if (prev.val > node.val) {
+            if (first == null) {
+                first = prev;
+            }
+            second = node;
+        }
+        prev = node;
+        inorder(node.right);
+    }
+}
