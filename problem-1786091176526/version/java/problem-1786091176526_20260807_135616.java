51ffc53929c0eb887c33ed857fd35292b19c6dc3
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-08-07 13:56:17 +0530

    Create

---
+++ problem-1786091176526/version/java/problem-1786091176526_20260807_135616.java
@@ -0,0 +1,25 @@
+// Last updated: 8/7/2026, 1:56:16 PM
+1class Solution {
+2    public int smallestNumber(int n, int t) {
+3        int current = n;
+4        while (true) {
+5            if (isDivisibleDigitProduct(current, t)) {
+6                return current;
+7            }
+8            current++;
+9        }
+10    }
+11    
+12    private boolean isDivisibleDigitProduct(int num, int t) {
+13        int product = 1;
+14        int temp = num;
+15        
+16        while (temp > 0) {
+17            int digit = temp % 10;
+18            product *= digit;
+19            temp /= 10;
+20        }
+21        
+22        return product % t == 0;
+23    }
+24}
