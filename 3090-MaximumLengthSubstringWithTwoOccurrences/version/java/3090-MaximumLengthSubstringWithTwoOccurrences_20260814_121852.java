c6ce202d44683230666e1ac02964e3c869c97333
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-08-14 12:18:53 +0530

    Create 3090-MaximumLengthSubstringWithTwoOccurrences

---
+++ 3090-MaximumLengthSubstringWithTwoOccurrences/version/java/3090-MaximumLengthSubstringWithTwoOccurrences_20260814_121852.java
@@ -0,0 +1,18 @@
+// Last updated: 8/14/2026, 12:18:52 PM
+1class Solution {
+2    public int maximumLengthSubstring(String s) {
+3        int res = 0;
+4        int[] fq = new int[26];
+5
+6        for (int l = 0, r = 0; r < s.length(); r++) {
+7            fq[(s.charAt(r) & 31) - 1]++;
+8
+9            while (fq[(s.charAt(r) & 31) - 1] > 2)
+10                fq[(s.charAt(l++) & 31) - 1]--;
+11
+12            res = Math.max(res, r - l + 1);
+13        }
+14
+15        return res;
+16    }
+17}
