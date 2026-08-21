4926c1e325f3a1ef818890496dc9cf3bdcf7916c
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-08-21 11:13:34 +0530

    Create 2884-ModifyColumns

---
+++ 2884-ModifyColumns/version/java/2884-ModifyColumns_20260821_111334.java
@@ -0,0 +1,15 @@
+// Last updated: 8/21/2026, 11:13:34 AM
+1class Solution {
+2    public int[] findDegrees(int[][] matrix) {
+3        int V = matrix.length;
+4        int[] degree = new int[V];
+5        for (int i = 0; i < V; i++) {
+6            for (int j = 0; j < matrix[i].length; j++) {
+7                if (matrix[i][j] == 1) {
+8                    degree[j]++;
+9                }
+10            }
+11        }
+12        return degree;
+13    }
+14}
