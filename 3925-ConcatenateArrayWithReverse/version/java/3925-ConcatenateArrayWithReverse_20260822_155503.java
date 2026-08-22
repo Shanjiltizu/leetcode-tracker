78172641f31f8a98e3cf2614ea36f2e74b64ac41
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-08-22 15:55:02 +0530

    Create 3925-ConcatenateArrayWithReverse

---
+++ 3925-ConcatenateArrayWithReverse/version/java/3925-ConcatenateArrayWithReverse_20260822_155503.java
@@ -0,0 +1,11 @@
+// Last updated: 8/22/2026, 3:55:03 PM
+1class Solution {
+2    public int[] concatWithReverse(int[] nums) {
+3        int [] sol = Arrays.copyOf(nums,nums.length*2);
+4        int index = nums.length;
+5        for(int i = index-1; i >= 0; i--){
+6            sol[index++] = nums[i];
+7        }
+8         return sol;
+9    }
+10}
