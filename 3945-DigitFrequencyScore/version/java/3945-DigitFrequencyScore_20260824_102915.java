be90884c3133a92e0c1c72157c68b503e16a108e
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-08-24 10:29:15 +0530

    Create 3945-DigitFrequencyScore

---
+++ 3945-DigitFrequencyScore/version/java/3945-DigitFrequencyScore_20260824_102915.java
@@ -0,0 +1,12 @@
+// Last updated: 24/08/2026, 10:29:15
+1class Solution {
+2    public int digitFrequencyScore(int n) {
+3        int score = 0;
+4        while(n>0){
+5            score+= n%10;
+6            n=n/10;
+7        }
+8        return score;
+9        
+10    }
+11}
