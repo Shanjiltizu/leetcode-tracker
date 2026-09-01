00e7131d53e3f4aace56a20039ddf1089690ab69
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-09-01 16:17:21 +0530

    Create 404-SumofLeftLeaves

---
+++ 404-SumofLeftLeaves/version/java/404-SumofLeftLeaves_20260901_161720.java
@@ -0,0 +1,18 @@
+// Last updated: 01/09/2026, 16:17:20
+1class Solution {
+2    public int sumOfLeftLeaves(TreeNode root) {
+3        return dfs(root, false);
+4    }
+5    
+6    private int dfs(TreeNode node, boolean isLeft) {
+7        if (node == null) {
+8            return 0;
+9        }
+10        if (node.left == null && node.right == null) { // Leaf node
+11            return isLeft ? node.val : 0;
+12        }
+13        int leftSum = dfs(node.left, true);   // Traverse left child
+14        int rightSum = dfs(node.right, false); // Traverse right child
+15        return leftSum + rightSum;
+16    }
+17}
