d29f74ce9d0a1aaf437d7203875ba821cca4864f
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-09-01 13:54:24 +0530

    Create 4271-FindTheDegreeOfEachVertex

---
+++ 4271-FindTheDegreeOfEachVertex/4271-FindTheDegreeOfEachVertex.java
@@ -0,0 +1,15 @@
+// Last updated: 01/09/2026, 13:54:24
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+        int V = matrix.length;
+        int[] degree = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1) {
+                    degree[j]++;
+                }
+            }
+        }
+        return degree;
+    }
+}
