7b7edee9b370e6f89c8eec4d4e7b856ac11b4910
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-09-01 13:55:08 +0530

    Create 3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum

---
+++ 3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum/3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum.java
@@ -0,0 +1,27 @@
+// Last updated: 01/09/2026, 13:55:08
+class Solution {
+    public int missingInteger(int[] A) {
+        int sum = A[0];
+        boolean[] seen = new boolean[52];
+        boolean seq = true;
+
+        seen[A[0]] = true;
+
+        for (int i = 1; i < A.length; i++) {
+            if (seq && A[i] == A[i - 1] + 1)
+                sum += A[i];
+            else {
+                seq = false;
+                if (sum > 50)
+                    return sum;
+            }
+            seen[A[i]] = true;
+        }
+
+        for (int i = sum; i < 52; i++)
+            if (!seen[i])
+                return i;
+
+        return sum;
+    }
+}
