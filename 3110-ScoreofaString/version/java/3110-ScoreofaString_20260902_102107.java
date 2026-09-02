4d8feab72a21ead321537d98e02a95635aa158bb
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-09-02 10:21:08 +0530

    Create 3110-ScoreofaString

---
+++ 3110-ScoreofaString/version/java/3110-ScoreofaString_20260902_102107.java
@@ -0,0 +1,18 @@
+// Last updated: 9/2/2026, 10:21:07 AM
+1class Solution {
+2    public int scoreOfString(String s) {
+3        char[] chars = s.toCharArray();
+4        int score = 0;
+5
+6        for (int i = 0; i < chars.length - 1; i++) {
+7            int diff = chars[i] - chars[i + 1];
+8            if (diff >= 0) {
+9                score += diff;
+10            } else {
+11                score -= diff;
+12            }
+13        }
+14
+15        return score;
+16    }
+17}
