436d6800baa4a69dad06f2cde2710952b7ae4c17
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-09-10 09:20:00 +0530

    Create 2119-ANumberAfterADoubleReversal

---
+++ 2119-ANumberAfterADoubleReversal/2119-ANumberAfterADoubleReversal.java
@@ -0,0 +1,8 @@
+// Last updated: 9/10/2026, 9:20:00 AM
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        if (num == 0) return true;
+        if (num % 10 == 0) return false;
+        return true;
+    }
+}
