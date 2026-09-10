0ac4d46e1b6bb1a0121cdea87c3c2d795ec0da05
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-09-10 09:20:46 +0530

    Create 2269-FindTheKBeautyOfANumber

---
+++ 2269-FindTheKBeautyOfANumber/2269-FindTheKBeautyOfANumber.java
@@ -0,0 +1,17 @@
+// Last updated: 9/10/2026, 9:20:46 AM
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+        String s = String.valueOf(num);
+        int count = 0;
+
+        for (int i = 0; i <= s.length() - k; i++) {
+            int val = Integer.parseInt(s.substring(i, i + k));
+
+            if (val != 0 && num % val == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
