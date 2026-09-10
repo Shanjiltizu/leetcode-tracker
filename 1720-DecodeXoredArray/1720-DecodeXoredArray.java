d1ea657d810943c968de8ee6dd1c43b393b958db
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-09-10 09:20:18 +0530

    Create 1720-DecodeXoredArray

---
+++ 1720-DecodeXoredArray/1720-DecodeXoredArray.java
@@ -0,0 +1,13 @@
+// Last updated: 9/10/2026, 9:20:18 AM
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int n = encoded.length;
+        int arr[] = new int[n+1];
+        arr[0]=first;
+        for(int i=1;i<=n;i++)
+            {
+                arr[i]=arr[i-1]^encoded[i-1];
+            }
+        return arr;
+    }
+}
