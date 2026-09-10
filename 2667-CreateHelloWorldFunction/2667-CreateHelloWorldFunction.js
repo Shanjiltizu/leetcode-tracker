11d1d6740dcbadbaae2248aa83935a13bf58c47a
SHANJIL TIZU <161858464+Shanjiltizu@users.noreply.github.com>
2026-09-10 09:19:06 +0530

    Create 2667-CreateHelloWorldFunction

---
+++ 2667-CreateHelloWorldFunction/2667-CreateHelloWorldFunction.js
@@ -0,0 +1,16 @@
+// Last updated: 9/10/2026, 9:19:06 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
